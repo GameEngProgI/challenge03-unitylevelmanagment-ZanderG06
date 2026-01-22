e88fc0f749819ca8434259289fa243cbd4e17024
Zander Goodwin <zanderngoodwin@gmail.com>
2026-01-22 14:14:38 -0400

    All Done

--- Assets/Systems/Managers/LevelChangeTrigger.cs
+++ Assets/Systems/Managers/LevelChangeTrigger.cs
@@ -4,8 +4,12 @@ public class LevelChangeTrigger : MonoBehaviour
 {
     public LevelManager levelManager;
 
+    public GameObject nextLevel;
+    public GameObject currentLevel;
+    public Transform spawn;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player")) levelManager.LoadLevel();
+        if(collision.CompareTag("Player")) levelManager.LoadLevel(nextLevel, currentLevel, spawn);
     }
 }
--- Assets/Systems/Managers/LevelManager.cs
+++ Assets/Systems/Managers/LevelManager.cs
@@ -3,30 +3,13 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    public GameObject level1;
-    public GameObject level2;
-
-    private bool isLevel1 = true;
-
     public GameObject player;
-    public Transform spawnPoint1;
-    public Transform spawnPoint2;
-    
-    public void LoadLevel()
+
+
+    public void LoadLevel(GameObject nextLevel, GameObject currentLevel, Transform spawn)
     {
-        if (isLevel1)
-        {
-            level1.SetActive(false);
-            level2.SetActive(true);
-            isLevel1 = false;
-            player.transform.position = spawnPoint2.position;
-        }
-        else
-        {
-            level1.SetActive(true);
-            level2.SetActive(false);
-            isLevel1 = true;
-            player.transform.position = spawnPoint1.position;
-        }
+        nextLevel.SetActive(true);
+        currentLevel.SetActive(false);
+        player.transform.position = spawn.position;
     }
 }
